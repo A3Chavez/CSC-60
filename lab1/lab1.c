b0091693e53a70bde3a23af770529470aa4c6412
Mushuooshu <34972840+Mushuooshu@users.noreply.github.com>
2019-09-27 08:26:55 -0700

    Add files via upload

---
+++ lab1/lab1.c
@@ -0,0 +1,17 @@
+/*------------------*/
+/* Anthony Chavez   */
+/* Lab 1            */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("\nLab 1. \n\n");
+    printf("Hello World.\n");
+    printf("Hi, Anthony Chavez.\n\n");
+    printf("No! Try not! Do or do not, there is no try.\n");
+    printf("- Yoda\n\n");
+
+    return EXIT_SUCCESS;
+}
+/*------------------*/
---
+++ lab3/lab3.c
@@ -0,0 +1,21 @@
+/*----------------*/
+/* Anthony Chavez */
+/* Lab 3          */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int age = 22;
+	double average = 246.23689;
+
+	printf("\nAnthony Chavez. Lab 3.\n");
+	printf("\nJane went to the store.\n");
+	printf("\nMy friend is %d years old. \n", age);
+	printf("\n\"So many books, so little time.\"\n- Frank Zappa\n");
+	printf("\nThe average = %.1f\n", average);
+	printf("\nThe average = %.3f\n", average);
+	printf("\nThe average = %.2e\n\n", average);
+
+	return EXIT_SUCCESS;
+}
